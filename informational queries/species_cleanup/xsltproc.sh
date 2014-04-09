d2182bdb3eca9f946c1d8c0b706fe337a03192e6
Scott Rohde <srohde@igb.illinois.edu>
2014-04-09 17:18:28 -0500

    Output STDERR from xsltproc to a file.

--- informational queries/species_cleanup/xsltproc.sh
+++ informational queries/species_cleanup/xsltproc.sh
@@ -2,6 +2,6 @@
 
 tmp=/tmp/out.$RANDOM.html
 
-xsltproc -o $tmp summary.xsl duplicate_species_local.xml
+xsltproc -o $tmp summary.xsl duplicate_species_local.xml 2> delete_duplicate_species.sql
 
 open -a 'Google Chrome' $tmp
