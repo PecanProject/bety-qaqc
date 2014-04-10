7e706f2329f0baff9ae9681a673341182b94e219
Scott Rohde <srohde@igb.illinois.edu>
2014-04-10 11:19:22 -0500

    Drafted command to run the query that produces the XML source file for the XSLT script.

--- informational queries/species_cleanup/xsltproc.sh
+++ informational queries/species_cleanup/xsltproc.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# draft: psql -At ebi_production_copy < duplicate_species_query.sql > duplicate_species_local.xml
+
 tmp=/tmp/out.$RANDOM.html
 
 xsltproc -o $tmp summary.xsl duplicate_species_local.xml 2> delete_duplicate_species.sql
