19da246ba7247187201208552c127c3ef544a044
Scott Rohde <srohde@igb.illinois.edu>
2014-05-02 17:20:18 -0500

    Fleshed out restrict_range function.

--- constraints/trigger_functions/restrict_range.sql
+++ constraints/trigger_functions/restrict_range.sql
@@ -1,29 +1,34 @@
-CREATE OR REPLACE FUNCTION restrict_range()
-  RETURNS TRIGGER AS $$
+CREATE OR REPLACE FUNCTION restrict_trait_range() RETURNS TRIGGER AS $$
+    DECLARE name varchar;
+    DECLARE min float;
+    DECLARE max float;
 BEGIN
-  IF COALESCE (
-    NEW.mean < (
-        SELECT
-            CAST (min AS numeric)
-        FROM
-            variables
-        WHERE
-            variables.id = NEW.variable_id
-    ),
-    TRUE
-  ) OR COALESCE (
-    NEW.mean > (
-        SELECT
-            CAST (max AS numeric)
-        FROM
-            variables
-        WHERE
-            variables.id = NEW.variable_id
-    ),
-    TRUE
-  ) THEN
-    raise EXCEPTION 'The value of mean is outside the allowed range for the variable type specified' ;
-  END IF;
-  RETURN NEW ;
+    SELECT
+
+
+        -- If min and max are constrained to be non-null, then the
+        -- COALESCE call is not needed.  In this case, some very large
+        -- number would be used for unconstrained maximimum values and
+        -- some large negative number would be used for unconstrained
+        -- minimum values.  Alternatively, the type could be changed
+        -- to float so that values '-infinity' and 'infinity' could be
+        -- used.  In any case, the min and max columns should probably
+        -- at least be altered to some numeric type.
+
+        -- If min, max and mean are all altered to be of the same
+        -- type, then the casts will not be needed.
+        
+        -- Treat NULLs as if they were infinity.        
+        variables.name, CAST(COALESCE(variables.min, '-infinity') AS float), CAST(COALESCE(variables.max, 'infinity') AS float) INTO name, min, max
+    FROM
+        variables
+    WHERE
+        variables.id = NEW.variable_id;
+    IF
+        NEW.mean::float < min OR NEW.mean::float > max
+    THEN
+        RAISE EXCEPTION 'The value of mean for trait % must be between % and %.', name, min::text, max::text;
+    END IF;
+    RETURN NEW ;
 END;
 $$ LANGUAGE plpgsql;
