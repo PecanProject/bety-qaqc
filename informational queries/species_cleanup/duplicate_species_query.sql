bd3426540f7dbf58a4d6981f7f945d1bf84f4528
Scott Rohde <srohde@igb.illinois.edu>
2015-03-02 15:34:58 -0600

    Added comments. Deleted commented-out code. Changed quoting style form single quote to $$. Added two columns to query results to help with figuring out which rows to get rid of.

--- informational queries/species_cleanup/duplicate_species_query.sql
+++ informational queries/species_cleanup/duplicate_species_query.sql
@@ -1,19 +1,58 @@
-SELECT query_to_xml('
+SELECT query_to_xml($$
 
 SELECT
-	s.*, array_to_string(ARRAY_AGG(DISTINCT T . ID), '', '') AS "linked_traits",
-	array_to_string(ARRAY_AGG(DISTINCT y. ID), '', '') AS "linked_yields",
-	array_to_string(ARRAY_AGG(DISTINCT C . ID), '', '') AS "linked_cultivars",
-	array_to_string(
-		ARRAY_AGG (DISTINCT ps.pft_id),
-		'', ''
-	) AS "linked_pfts"
+	s.*, array_to_string(ARRAY_AGG(DISTINCT t.id), ', ') AS linked_traits,
+	array_to_string(ARRAY_AGG(DISTINCT y.id), ', ') AS linked_yields,
+	array_to_string(ARRAY_AGG(DISTINCT c.id), ', ') AS linked_cultivars,
+	array_to_string(ARRAY_AGG (DISTINCT ps.pft_id), ', ') AS linked_pfts,
+    /* Mark for deletion any row such that:
+       (1) there are no linked traits, yields, or cultivars;
+       (2) there is another row:
+           (a)  whose set of linked pfts properly includes this row's set of linked pfts, or
+                whose set of linked pfts is equal to this row's set of linked pfts and either that row has some linked traits, yields, or cultivars, or that row has a lower id number than this row;
+           (b) whose genus value matches this row if this row's genus value is non-empty;
+           (c) whose species value matches this row if this row's species value is non-empty;
+           (d) whose commonname value matches this row if this row's commonname value is non-empty;
+           (e) whose AcceptedSymbol value matches this row if this row's AcceptedSymbol value is non-empty;
+           (f) whose spcd value matches this row if this row's spcd value is non-NULL. */
+    ((ARRAY_LENGTH(ARRAY_REMOVE(ARRAY_AGG(t.id), NULL), 1) IS NULL) AND
+    (ARRAY_LENGTH(ARRAY_REMOVE(ARRAY_AGG(y.id), NULL), 1) IS NULL) AND
+    (ARRAY_LENGTH(ARRAY_REMOVE(ARRAY_AGG(c.id), NULL), 1) IS NULL) AND
+     (SELECT EXISTS(SELECT 1 FROM species s2 LEFT JOIN pfts_species ps2 ON ps2.specie_id = s2.id
+             WHERE s2.scientificname = s.scientificname AND s2.id != s.id GROUP BY s2.id
+             HAVING (
+                     ARRAY_AGG(DISTINCT ps2.pft_id) @> ARRAY_AGG(DISTINCT ps.pft_id)
+                 AND (ARRAY_AGG(DISTINCT ps2.pft_id) != ARRAY_AGG(DISTINCT ps.pft_id)
+                   OR (SELECT COUNT(*) FROM traits WHERE specie_id = s2.id) > 0
+                   OR (SELECT COUNT(*) FROM yields WHERE specie_id = s2.id) > 0
+                   OR (SELECT COUNT(*) FROM cultivars WHERE specie_id = s2.id) > 0
+                   OR s2.id < s.id)
+                 AND (s.genus = '' OR s2.genus = s.genus)
+                 AND (s.species = '' OR s2.species = s.species)
+                 AND (s.commonname = '' OR s2.commonname = s.commonname)
+                 AND (s."AcceptedSymbol" = '' OR s2."AcceptedSymbol" = s."AcceptedSymbol")
+                 AND (s.spcd IS NULL OR s2.spcd = s.spcd))
+                    ))) AS can_delete,
+    /* Mark as a deletion candidate rows that satisfy conditions (1) through (2b): */
+    ((ARRAY_LENGTH(ARRAY_REMOVE(ARRAY_AGG(t.id), NULL), 1) IS NULL) AND
+    (ARRAY_LENGTH(ARRAY_REMOVE(ARRAY_AGG(y.id), NULL), 1) IS NULL) AND
+    (ARRAY_LENGTH(ARRAY_REMOVE(ARRAY_AGG(c.id), NULL), 1) IS NULL) AND
+     (SELECT EXISTS(SELECT 1 FROM species s2 LEFT JOIN pfts_species ps2 ON ps2.specie_id = s2.id
+             WHERE s2.scientificname = s.scientificname AND s2.id != s.id GROUP BY s2.id
+             HAVING (
+                     ARRAY_AGG(DISTINCT ps2.pft_id) @> ARRAY_AGG(DISTINCT ps.pft_id)
+                 AND (ARRAY_AGG(DISTINCT ps2.pft_id) != ARRAY_AGG(DISTINCT ps.pft_id)
+                   OR (SELECT COUNT(*) FROM traits WHERE specie_id = s2.id) > 0
+                   OR (SELECT COUNT(*) FROM yields WHERE specie_id = s2.id) > 0
+                   OR (SELECT COUNT(*) FROM cultivars WHERE specie_id = s2.id) > 0
+                   OR s2.id < s.id))
+                    ))) AS deletion_candidate
 FROM
 	species s
-LEFT JOIN traits T ON T .specie_id = s."id"
-LEFT JOIN yields y ON y.specie_id = s."id"
-LEFT JOIN cultivars C ON C .specie_id = s."id"
-LEFT JOIN pfts_species ps ON ps.specie_id = s. ID
+LEFT JOIN traits t ON t.specie_id = s.id
+LEFT JOIN yields y ON y.specie_id = s.id
+LEFT JOIN cultivars c ON c.specie_id = s.id
+LEFT JOIN pfts_species ps ON ps.specie_id = s.id
 WHERE
 	scientificname IN (
 		SELECT
@@ -21,100 +60,16 @@ WHERE
 		FROM
 			species
 		GROUP BY
-			scientificname/*,
-			spcd,
-			genus,
-			species,
-			"AcceptedSymbol",
-			"SynonymSymbol",
-			"Symbol",
-			"PLANTS_Floristic_Area",
-			"State",
-			"Category",
-			"Family",
-			"FamilySymbol",
-			"FamilyCommonName",
-			"xOrder",
-			"SubClass",
-			"Class",
-			"SubDivision",
-			"Division",
-			"SuperDivision",
-			"SubKingdom",
-			"Kingdom",
-			"ITIS_TSN",
-			"Duration",
-			"GrowthHabit",
-			"NativeStatus",
-			"NationalWetlandIndicatorStatus",
-			"RegionalWetlandIndicatorStatus",
-			"ActiveGrowthPeriod",
-			"AfterHarvestRegrowthRate",
-			"Bloat",
-			"C2N_Ratio",
-			"CoppicePotential",
-			"FallConspicuous",
-			"FireResistance",
-			"FoliageTexture",
-			"GrowthForm",
-			"GrowthRate",
-			"MaxHeight20Yrs",
-			"MatureHeight",
-			"KnownAllelopath",
-			"LeafRetention",
-			"Lifespan",
-			"LowGrowingGrass",
-			"NitrogenFixation",
-			"ResproutAbility",
-			"AdaptedCoarseSoils",
-			"AdaptedMediumSoils",
-			"AdaptedFineSoils",
-			"AnaerobicTolerance",
-			"CaCO3Tolerance",
-			"ColdStratification",
-			"DroughtTolerance",
-			"FertilityRequirement",
-			"FireTolerance",
-			"MinFrostFreeDays",
-			"HedgeTolerance",
-			"MoistureUse",
-			"pH_Minimum",
-			"pH_Maximum",
-			"Min_PlantingDensity",
-			"Max_PlantingDensity",
-			"Precipitation_Minimum",
-			"Precipitation_Maximum",
-			"RootDepthMinimum",
-			"SalinityTolerance",
-			"ShadeTolerance",
-			"TemperatureMinimum",
-			"BloomPeriod",
-			"CommercialAvailability",
-			"FruitSeedPeriodBegin",
-			"FruitSeedPeriodEnd",
-			"Propogated_by_BareRoot",
-			"Propogated_by_Bulbs",
-			"Propogated_by_Container",
-			"Propogated_by_Corms",
-			"Propogated_by_Cuttings",
-			"Propogated_by_Seed",
-			"Propogated_by_Sod",
-			"Propogated_by_Sprigs",
-			"Propogated_by_Tubers",
-			"Seeds_per_Pound",
-			"SeedSpreadRate",
-			"SeedlingVigor"*/
+			scientificname
 		HAVING
-			COUNT (*) > 1
-		AND NOT scientificname = ''''
+			COUNT(*) > 1
+		AND NOT scientificname = ''
 		AND scientificname IS NOT NULL
 	)
 GROUP BY
-	s."id"
+	s.id
 ORDER BY
-	scientificname',
-
-
-    'f',
-    'f',
-    '');
+	scientificname$$,
+    'f', -- omit columns containing NULL from output
+    'f', -- output as top-level <tablename> element containing <row> elements
+    ''); -- empty target namespace
