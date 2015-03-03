451a60343cb9f3610c246362c0ab3a2d4222a7b7
Scott Rohde <srohde@igb.illinois.edu>
2015-03-03 10:38:23 -0600

    Added options to skip the SQL query or to do only the query. Added progress output.

--- informational queries/species_cleanup/xsltproc.sh
+++ informational queries/species_cleanup/xsltproc.sh
@@ -1,10 +1,35 @@
 #!/usr/bin/env bash
 
-# draft: psql -At ebi_production_copy < duplicate_species_query.sql > duplicate_species_local.xml
-psql -At -U bety -h ebi-forecast.igb.illinois.edu ebi_production < duplicate_species_query.sql > duplicate_species_local.xml
+while getopts 'sq' OPTION
+do
+    case $OPTION in
+        s) skip_query=1
+            ;;
+        q) query_only=1
+            ;;
+        ?) printf "Usage: %s: [-s]\n" $(basename $0) >&2
+            exit 2
+            ;;
+    esac
+done
 
-tmp=/tmp/out.$RANDOM.html
+if [ ! "$skip_query" ]; then
 
-xsltproc -o $tmp summary.xsl duplicate_species_local.xml 2> delete_duplicate_species.sql
+    echo "querying database ..."
+    psql -At -U bety -h ebi-forecast.igb.illinois.edu ebi_production < duplicate_species_query.sql > duplicate_species_local.xml
+    echo "done"
 
-open -a 'Google Chrome' $tmp
+fi
+
+if [ ! "$query_only" ]; then
+
+    tmp=/tmp/out.$RANDOM.html
+
+    echo "processing query output ..."
+    xsltproc -o $tmp summary.xsl duplicate_species_local.xml 2> delete_duplicate_species.sql
+    echo "done"
+
+    echo "opening browser to result ..."
+    open -a 'Google Chrome' $tmp
+
+fi
