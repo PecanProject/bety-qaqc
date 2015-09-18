2474514a9c6242c8876d502fe13424585ee01347
Scott Rohde <srohde@igb.illinois.edu>
2015-09-18 15:56:22 -0500

    Added to informational header.

--- informational_queries/citations_cleanup/find_duplicate_citations.sql
+++ informational_queries/citations_cleanup/find_duplicate_citations.sql
@@ -1,5 +1,6 @@
 \t
-SELECT 'This is a list of groups of citations having matching author, year, and title';
+SELECT 'This is a list of groups of citations having matching author, year, and title.';
+SELECT 'Some column values are truncated so that rows display on a single line.';
 \t
 SELECT c.id, c.author, c.year, SUBSTRING(c.title, 1, 10) AS "title",
     SUBSTRING(c.journal, 1, 10) AS "journal",
